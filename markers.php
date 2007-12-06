61c0d3d4de0ba71f582600008d7fe372a8c90ee4
conroy.whitney <conroy.whitney@3ec9f336-2a40-0410-9f04-cfe5fb82e8a9>
2007-12-06 19:50:22 +0000

    final checkin before demo; made images smaller; fixed some things; added gatech pics

--- markers.php
+++ markers.php
@@ -15,15 +15,16 @@
 	$end_dt = "$end 23:59.000";
 
 	$db = new mysqli("localhost", "root", $mysql_password, "4440");
-	$query = "SELECT * FROM images WHERE (taken_at >= '$start_dt' AND taken_at <= '$end_dt')";
+	$query = "SELECT * FROM images WHERE (taken_at >= '$start_dt' AND taken_at <= '$end_dt') ORDER BY filename ASC";
 
 	if ($result = $db->query($query)) {
 		while($row = $result->fetch_assoc()) {
 ?>
 			<marker lat="<?php echo $row['y']; ?>" lng="<?php echo $row['x']; ?>" 
-				html="&lt;img src=&quot;images/demo/<?php echo $row['filename']; ?>&quot; width=&quot;200&quot; /&gt;
+				html="&lt;img src=&quot;images/demo/<?php echo $row['filename']; ?>&quot; width=&quot;200&quot; height=&quot;150&quot; /&gt;
+					&lt;br&gt;&lt;b&gt;<?php echo $row['filename']; ?>&lt;/b&gt;
 					&lt;br&gt;Taken at: <?php echo format_date($row['taken_at']); ?>"
-				label="<?php echo $row['filename']; ?>" />
+				label="&lt;li&gt;<?php echo $row['filename']; ?>&lt;/li&gt;" />
 <?php
 		}
 	}
